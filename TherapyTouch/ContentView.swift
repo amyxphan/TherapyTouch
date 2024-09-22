35fd900b6b33be24c8f6a8505df4923fb8965856
amyxphan <phan.amy28@gmail.com>
2024-09-21 20:02:35 -0500

    Initial Commit

---
+++ TherapyTouch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TherapyTouch
+//
+//  Created by Amy Phan on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TherapyTouch/TherapyTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TherapyTouchApp.swift
+//  TherapyTouch
+//
+//  Created by Amy Phan on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TherapyTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
