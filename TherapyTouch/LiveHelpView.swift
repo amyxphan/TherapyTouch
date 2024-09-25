5ba56e5374ab30a380a322e7f28e3ce2e153b61b
amyxphan <phan.amy28@gmail.com>
2024-09-24 20:22:47 -0500

    basic ui for live help, need ot work on therapist name cards

--- TherapyTouch/LiveHelpView.swift
+++ TherapyTouch/LiveHelpView.swift
@@ -9,8 +9,85 @@ import SwiftUI
 struct LiveHelpView: View {
     var body: some View {
         NavigationView {
-            Text("LiveHelp")
-                .navigationTitle("Let's Talk!")
+            VStack(alignment: .leading) {
+                Text("Appointments:")
+                    .font(.system(size: 22))
+                HStack(alignment: .center) {
+                    Button(action: {
+                        // Button action here
+                    }, label: {
+                        Text("Book an appointment")
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: 150)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal)
+                            .foregroundColor(.black)
+                    })
+                    .background(Color(hex: "#F0DFBE"))
+                    .cornerRadius(5)
+                    .buttonBorderShape(.roundedRectangle)
+                    
+                    Button(action: {
+                        // Button action here
+                    }, label: {
+                        Text("Cancel an appointment")
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: 150)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal)
+                            .foregroundColor(.black)
+                    })
+                    .background(Color(hex: "#F0DFBE"))
+                    .cornerRadius(5)
+                    .buttonBorderShape(.roundedRectangle)
+                }
+                .padding(.bottom, 5)
+                Text("Get live help:")
+                    .font(.system(size: 22))
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Therapist Name Card")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 65)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Therapist Name Card")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 65)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Therapist Name Card")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 65)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+
+            }
+            .navigationTitle("Let's Talk!")
         }
     }
 }
--- TherapyTouch/ProductivityView.swift
+++ TherapyTouch/ProductivityView.swift
@@ -9,8 +9,109 @@ import SwiftUI
 struct ProductivityView: View {
     var body: some View {
         NavigationView {
-            Text("Productivity")
-                .navigationTitle("Productivity")
+            VStack(alignment: .leading) {
+                // need to make a functional add button to add more personalized goals
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Water")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Mood")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Weather")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Sleep")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Workout")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("No Spend")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Button(action: {
+                    // Button action here
+                }, label: {
+                    Text("Supplements")
+                        .frame(maxWidth: 325)
+                        .padding(.vertical, 18)
+                        .padding(.horizontal)
+                        .foregroundColor(.black)
+                })
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+            }
+            //edit title to be centered & uniform with home page
+        .navigationTitle("Productivity")
         }
     }
 }
