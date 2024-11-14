82e6692191624d007fc182297e083c22d24cbda8
amyxphan <phan.amy28@gmail.com>
2024-11-13 22:01:38 -0600

    updated weather page

--- TherapyTouch/ProductivityWeatherView.swift
+++ TherapyTouch/ProductivityWeatherView.swift
@@ -9,17 +9,32 @@ import SwiftUI
 
 struct ProductivityWeatherView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Weather")
-                    .font(.system(size: 28, weight: .bold))
-                    .padding(.top, 45)
-                Spacer()
-                Text("Weather Page")
-                Spacer()
+        VStack {
+            Text("Weather")
+                .font(.system(size: 28, weight: .bold))
+                .padding(.top, 20)
+            
+            Text("How's the weather today?")
+                .font(.system(size: 20))
+                .padding(.top, 20)
+            
+            HStack { // add weather icons here NEED TO DRAW
+//                Image("Mood1")
+//                    .resizable()
+//                    .frame(width: 60, height: 60)
+//                    .onTapGesture {
+//                        //update calendar
+//                    }
             }
-        }
-    }
+            
+            Text("This month:")
+                .font(.system(size: 22, weight: .bold))
+                .padding(.top, 40)
+            
+            Spacer()
+            
+            //add a monthly tracker here
+        }    }
 }
 
 #Preview {
