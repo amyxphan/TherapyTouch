43af36ebc5cd724951ac77386d322af85148f121
amyxphan <phan.amy28@gmail.com>
2024-11-13 21:55:33 -0600

    added icons to mood tracker

--- TherapyTouch/ContentView.swift
+++ TherapyTouch/ContentView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(hex: "#B89D6A")
-                .edgesIgnoringSafeArea(.all)
             TabView {
                 HomeView()
                     .tabItem {
--- TherapyTouch/ProductivityMoodView.swift
+++ TherapyTouch/ProductivityMoodView.swift
@@ -11,12 +11,54 @@ struct ProductivityMoodView: View {
     var body: some View {
         ScrollView {
             VStack {
-                Text("Mood")
+                Text("Mood Tracker")
                     .font(.system(size: 28, weight: .bold))
-                    .padding(.top, 45)
+                    .padding(.top, 20)
+                
+                Text("How are you feeling today?:")
+                    .font(.system(size: 20))
+                    .padding(.top, 20)
+                
+                HStack {
+                    Image("Mood1")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            //update calendar
+                        }
+                    Image("Mood2")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            //update calendar
+                        }
+                    Image("Mood3")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            //update calendar
+                        }
+                    Image("Mood4")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            //update calendar
+                        }
+                    Image("Mood5")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            //update calendar
+                        }
+                }
+                
                 Spacer()
-                Text("Mood Page")
-                Spacer()
+                
+                Text("This month:")
+                    .font(.system(size: 22, weight: .bold))
+                    .padding(.top, 40)
+                
+                //add a monthly tracker here
             }
         }
     }
