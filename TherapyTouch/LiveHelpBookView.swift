512475da674345b0e88c3ba7dc6b5b245d752b0f
amyxphan <phan.amy28@gmail.com>
2024-11-14 20:06:57 -0600

    edited UI for buttons to make sense + added pages to route to

---
+++ TherapyTouch/LiveHelpBookView.swift
@@ -0,0 +1,8 @@
+//
+//  LiveHelpBookView.swift
+//  TherapyTouch
+//
+//  Created by Amy Phan on 11/14/24.
+//
+
+import Foundation
---
+++ TherapyTouch/LiveHelpManageView.swift
@@ -0,0 +1,8 @@
+//
+//  LiveHelpManageView.swift
+//  TherapyTouch
+//
+//  Created by Amy Phan on 11/14/24.
+//
+
+import Foundation
