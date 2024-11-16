3ff47f7113b6dd14ba319df0a2b31d92809d7b35
amyxphan <phan.amy28@gmail.com>
2024-11-15 22:23:14 -0600

    added content to sleep page, need to work on bed icon slider

--- TherapyTouch/ProductivitySleepView.swift
+++ TherapyTouch/ProductivitySleepView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ProductivitySleepView: View {
+    @State private var count: Int = 0
+    private let sliderWidth: CGFloat = 300
     var body: some View {
         ScrollView {
             VStack {
                 Text("Sleep")
                     .font(.system(size: 28, weight: .bold))
                     .padding(.top, 45)
+                
                 Spacer()
-                Text("Sleep Page")
+
+                ZStack {
+                    Slider(
+                        value: Binding(
+                            get: { Double(count) },
+                            set: { count = Int($0) }
+                        ),
+                        in: 0...24,
+                        step: 1
+                    )
+                    .padding()
+
+                   Image("Bed")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+
+               }
+                
+                Text("Hours of Sleep: \(count)")
+                    .font(.title)
+                    .padding()
+                
                 Spacer()
+                
+                Text("This month:")
+                    .font(.system(size: 22, weight: .bold))
+                    .padding(.top, 100)
             }
         }
     }
