230aec10a1ee4c2ed74c438dac3aa8900dba0b97
amyxphan <phan.amy28@gmail.com>
2024-11-19 20:07:43 -0600

    added page to view appointments (dummy data)

--- TherapyTouch/LiveHelpManageView.swift
+++ TherapyTouch/LiveHelpManageView.swift
@@ -5,3 +5,161 @@
 //  Created by Amy Phan on 11/14/24.
 //
 
+import SwiftUI
+
+struct LiveHelpManageView: View {
+    var therapist: String = "Jane Doe"
+    var description: String = "Job Description"
+
+    var body: some View {
+        ScrollView {
+            Text("View my appointments")
+                .font(.system(size: 28, weight: .bold))
+                .padding(.top, 45)
+                .padding(.bottom, 40)
+            
+            VStack (alignment: .leading){
+                Text("Today:")
+                    .font(.system(size: 18))
+                    .padding(.leading, 5)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(therapist)
+                            .font(.system(size: 20, weight: .bold))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        Text(description)
+                            .font(.system(size: 18))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    VStack {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Join")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.white)
+                                .buttonBorderShape(.roundedRectangle)
+                                .padding(8)
+                                .background(Color(hex: "#B89D6A"))
+                                .frame(maxWidth: 90)
+                                .cornerRadius(5)
+                        })
+                        
+                    }
+                    .frame(maxHeight: 90)
+                    .padding(.trailing, 10)
+                }
+                .frame(maxWidth: 375, alignment: .leading)
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Text("Tomorrow:")
+                    .font(.system(size: 18))
+                    .padding(.leading, 5)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(therapist)
+                            .font(.system(size: 20, weight: .bold))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        Text(description)
+                            .font(.system(size: 18))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    VStack {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Join")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.white)
+                                .buttonBorderShape(.roundedRectangle)
+                                .padding(8)
+                                .background(Color(hex: "#B89D6A"))
+                                .frame(maxWidth: 90)
+                                .cornerRadius(5)
+                        })
+                        
+                    }
+                    .frame(maxHeight: 90)
+                    .padding(.trailing, 10)
+                }
+                .frame(maxWidth: 375, alignment: .leading)
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+                
+                Text("Next Week:")
+                    .font(.system(size: 18))
+                    .padding(.leading, 5)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(therapist)
+                            .font(.system(size: 20, weight: .bold))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        Text(description)
+                            .font(.system(size: 18))
+                            .padding(.leading, 8)
+                            .padding(.bottom, 2)
+                            .foregroundColor(.black)
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    VStack {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Join")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.white)
+                                .buttonBorderShape(.roundedRectangle)
+                                .padding(8)
+                                .background(Color(hex: "#B89D6A"))
+                                .frame(maxWidth: 90)
+                                .cornerRadius(5)
+                        })
+                        
+                    }
+                    .frame(maxHeight: 90)
+                    .padding(.trailing, 10)
+                }
+                .frame(maxWidth: 375, alignment: .leading)
+                .background(Color(hex: "#F0DFBE"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 5)
+            }
+        }
+    }
+
+}
+
+struct LiveHelpManageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LiveHelpManageView()
+    }
+}
+
+
