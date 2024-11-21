586036d37ac584765dd20097477e4228949e6342
amyxphan <phan.amy28@gmail.com>
2024-11-20 19:41:17 -0600

    added base line for step count

--- TherapyTouch/ProductivitySupplementView.swift
+++ TherapyTouch/ProductivitySupplementView.swift
@@ -15,7 +15,31 @@ struct ProductivityStepsView: View {
                     .font(.system(size: 28, weight: .bold))
                     .padding(.top, 45)
                 Spacer()
-                Text("Steps Page")
+                Text("Did you reach your step goal?")
+                    .font(.system(size: 20))
+                    .padding(.bottom, 10)
+                Spacer()
+                NavigationLink(destination: ProductivityStepsView()) {
+                    Text("Add/Update Goal")
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 10)
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(.black)
+                }
+                .background(Color(hex: "#B89D6A"))
+                .cornerRadius(5)
+                .buttonBorderShape(.roundedRectangle)
+                .padding(.bottom, 50)
+                Image("Steps")
+                    .resizable()
+                    .frame(maxWidth: 170, maxHeight: 250)
+                    .padding(.bottom, 50)
+                
+                Text("Current Count: XX")
+                    .font(.system(size: 20, weight: .bold))
+                Spacer()
+                Text("Current Goal: XX")
+                    .font(.system(size: 20, weight: .bold))
                 Spacer()
             }
         }
