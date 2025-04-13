72e5d3b8d8a973024ed4fab335dafa8a985cb497
amyxphan <phan.amy28@gmail.com>
2025-04-13 12:31:33 -0500

    firebase connection pt2

--- TherapyTouch/AppDelegate.swift
+++ TherapyTouch/AppDelegate.swift
@@ -5,4 +5,18 @@
 //  Created by Amy Phan on 4/13/25.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+@main
+struct MyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- TherapyTouch/TherapyTouchApp.swift
+++ TherapyTouch/TherapyTouchApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TherapyTouchApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
